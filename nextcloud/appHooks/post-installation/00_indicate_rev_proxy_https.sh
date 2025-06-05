6a56c0ec5560d556c7683d29ffae353720d2c1d9
Nagasudhir <nagasudhirpulla@gmail.com>
2025-06-05 08:48:49 +0530

    https rev proxy command in post install hook script

---
+++ nextcloud/appHooks/post-installation/00_indicate_rev_proxy_https.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+php /var/www/html/occ config:system:set overwriteprotocol --value=https
--- nextcloud/start.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-bash /entrypoint.sh
-# TODO comment out exec "$@" in entrypoint using sed
-su -www-data -c "php occ config:system:set overwriteprotocol --value=https"
-exec "$@"

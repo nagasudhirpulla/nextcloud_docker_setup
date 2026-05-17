bdaf51706bfe017865da58c82862c70deb74ac34
Nagasudhir <nagasudhirpulla@gmail.com>
2026-05-17 14:32:26 +0530

    changes in reverse proxy and config

--- nextcloud/appHooks/post-installation/00_indicate_rev_proxy_https.sh
+++ nextcloud/appHooks/post-installation/00_indicate_rev_proxy_https.sh
@@ -1,8 +1,21 @@
-php occ config:system:set overwriteprotocol --value=https
+# 1. Enforce HTTPS for all generated Nextcloud absolute URLs
+php occ config:system:set overwriteprotocol --value="https"
+
+# 2. Dynamically fetch the proxy container IP and append it safely to trusted_proxies
 proxy_ip=$(getent hosts proxy | awk '{print $1}')
-php occ config:system:set trusted_proxies 0 --value=$proxy_ip/32
-# php occ config:system:set trusted_proxies 1 --value=127.0.0.1
+php occ config:system:set trusted_proxies 0 --value="$proxy_ip"
+php occ config:system:set trusted_proxies 1 --value="127.0.0.1"
+php occ config:system:set trusted_proxies 2 --value="app"
+
+# 3. Lock down the public-facing domain name
 php occ config:system:set overwritehost --value="$NEXTCLOUD_FQDN"
+
+# 4. Ensure background cron jobs generate correct URLs in emails/notifications
 php occ config:system:set overwrite.cli.url --value="https://$NEXTCLOUD_FQDN"
-php occ config:system:set forwarded_for_headers 0 --value="HTTP_X_FORWARDED_FOR"
-# php occ config:system:set overwritewebroot --value="/"
+
+# 5. Tell Nextcloud to read client IPs from the Apache proxy header safely
+php occ config:system:set forwarded_for_headers --value="HTTP_X_FORWARDED_FOR"
+
+# php occ config:system:set trusted_proxies 1 --value=127.0.0.1
+
+# # config.php location is /var/www/html/config/config.php please check here if the values are set correctly 
--- nextcloud/appHooks/post-installation/01_install_apps.sh
+++ nextcloud/appHooks/post-installation/01_install_apps.sh
@@ -1,29 +1,29 @@
-# set default app to dashboard
-php occ config:system:set defaultapp --value=dashboard
+# # set default app to dashboard
+# php occ config:system:set defaultapp --value=dashboard
 
-# install nextcloud office app
+# # install nextcloud office app
 # php occ app:install richdocuments
 
-# set collabora online server URL
+# # set collabora online server URL
 # php occ config:app:set richdocuments wopi_url --value="https://$COLLABORA_FQDN"
 
-# install nextcloud talk app
+# # install nextcloud talk app
 # php occ app:install spreed
 
-# install nextcloud drawio app
+# # install nextcloud drawio app
 # php occ app:install drawio
 
-# install nextcloud built in code server
+# # install nextcloud built in code server
 # php -d memory_limit=512M occ app:install richdocumentscode
 
-# occ commands for nextcloud talk docker image - https://nextcloud-talk.readthedocs.io/en/latest/occ/#talksignalingadd
-# add high performance backend (signaling server) for nextcloud talk
+# # occ commands for nextcloud talk docker image - https://nextcloud-talk.readthedocs.io/en/latest/occ/#talksignalingadd
+# # add high performance backend (signaling server) for nextcloud talk
 # php occ talk:signaling:add "https://${SIGNAL_FQDN}" "${SIGNALING_SECRET}"
 
-# add stun server for nextcloud talk
+# # add stun server for nextcloud talk
 # php occ talk:stun:add "${SIGNAL_FQDN}:3478"
-# remove default stun server
+# # remove default stun server
 # php occ talk:stun:delete "stun.nextcloud.com:443"
 
-# configure turn server for nextcloud talk
+# # configure turn server for nextcloud talk
 # php occ talk:turn:add "turn" "${SIGNAL_FQDN}:3478" "udp,tcp" --secret="${TURN_SECRET}"

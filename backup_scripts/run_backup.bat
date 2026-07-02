760bb372bee45ed24e1b3e61eab75b5cfee02a2e
Nagasudhir <nagasudhirpulla@gmail.com>
2026-07-02 08:49:58 +0530

    restore script done

--- backup_scripts/run_backup.bat
+++ backup_scripts/run_backup.bat
@@ -4,11 +4,18 @@ cd /d "%~dp0"
 
 CALL config.bat
 
+echo ===================================================
+echo   NEXTCLOUD & POSTGRESQL BACKUP SCRIPT
+echo ===================================================
+echo.
+
 echo [1/6] Enabling Nextcloud Maintenance Mode...
 docker exec -u www-data %NEXTCLOUD_CONTAINER% php occ maintenance:mode --on
+echo.
 
 echo [2/6] Create a clean Database Dump directly into the DB storage volume
 docker exec %DB_CONTAINER% pg_dump -U %DB_USER% -d %DB_NAME% -F c -f /var/lib/postgresql/nextcloud_backup.dump
+echo.
 
 echo [3/6] Running the daily deduplicated backup (Files + Database logical backup file)...
 :: This maps both volumes into a single /data directory inside the Restic container
@@ -21,9 +28,11 @@ docker run --rm ^
   -r /repo backup ^
   /data/nextcloud_files ^
   /data/postgres_raw_data/nextcloud_backup.dump
+echo.
 
 echo [4/6] Disabling Nextcloud Maintenance Mode...
 docker exec -u www-data %NEXTCLOUD_CONTAINER% php occ maintenance:mode --off
+echo.
 
 echo [5/6] Pruning old data (Keeping last 365 days)...
 docker run --rm ^
@@ -32,8 +41,10 @@ docker run --rm ^
   restic/restic ^
   -r /repo forget ^
   --keep-within 365d --prune
+echo.
 
 echo [6/6] Cleaning up temporary dump file from the live DB volume
 docker exec %DB_CONTAINER% rm /var/lib/postgresql/nextcloud_backup.dump
+echo.
 
 echo Backup process complete! Both volumes are securely stored on your Windows host.
